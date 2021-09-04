3e377a32df15576c6f0814d01cb89fc0e70d78de
Ben De Leon <bendeleonjr@hotmail.com>
2021-09-04 15:53:55 +0800

    using range

---
+++ 014_using_range.py
@@ -0,0 +1,16 @@
+print("Range that prints 0 to 9")
+x = range(10)
+for n in x:
+  print(n)
+
+print("Range that prints 5 to 9")
+# prints 5 to 9
+y = range(5,10)
+for n in y:
+  print(n)
+
+print("Range that prints 5, 7, and 9")
+# prints 5 to 9
+y = range(5,10,2)
+for n in y:
+  print(n)
